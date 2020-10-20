0ec81b5f797bf5ebf2f868fec8092acd9c5e58a4
Weng Fei Fung <weffung@ucdavis.edu>
2020-10-19 20:49:24 -0700

    Password Generator object with character sets

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,26 @@
 // Assignment code here
+var PasswordGenerator = {
+  characterSets: {
+    special: " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    numerals: "0123456789"
+  },
+  ask() {
 
+  },
+  write() {
+    return "Placeholder password";
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
 }
 
